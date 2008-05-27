8ee7886df60a3ea94bf4d9f4cf09ff5b46bd3151
tatebe <tatebe@4a5e1ece-6a47-475c-b646-731d720b41d0>
2008-05-27 14:59:10 +0000

    Gfarm version 2.1.0 release

---
+++ gfarm_v2/tags/gfarm_2_1_0/include/gfarm/gfarm.h
@@ -0,0 +1,9 @@
+#include <gfarm/gfarm_config.h>
+#include <gfarm/error.h>
+#include <gfarm/gfarm_misc.h>
+#include <gfarm/gfarm_stringlist.h>
+#include <gfarm/host_info.h>
+#include <gfarm/user_info.h>
+#include <gfarm/group_info.h>
+#include <gfarm/gfs_glob.h>
+#include <gfarm/gfs.h>
