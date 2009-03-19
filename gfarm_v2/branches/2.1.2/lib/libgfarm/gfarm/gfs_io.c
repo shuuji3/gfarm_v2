c11c43ef29fa1a00464fc1eb5a40cd2cfda8bc57
tatebe <tatebe@4a5e1ece-6a47-475c-b646-731d720b41d0>
2009-03-19 14:25:49 +0000

    version 2.1.2 release branch

---
+++ gfarm_v2/branches/2.1.2/lib/libgfarm/gfarm/gfs_io.c
@@ -0,0 +1,201 @@
+#include <assert.h>
+#include <stdio.h>	/* config.h needs FILE */
+#include <unistd.h>
+
+#define GFARM_INTERNAL_USE /* GFARM_FILE_LOOKUP */
+#include <gfarm/gfarm.h>
+
+#include "gfutil.h"
+
+#include "gfm_client.h"
+#include "config.h"
+#include "lookup.h"
+#include "gfs_io.h"
+
+static gfarm_error_t
+gfm_open_flag_check(int flag)
+{
+	if (flag & ~GFARM_FILE_USER_MODE)
+		return (GFARM_ERR_INVALID_ARGUMENT);
+	if ((flag & GFARM_FILE_ACCMODE) == GFARM_FILE_LOOKUP)
+		return (GFARM_ERR_INVALID_ARGUMENT);
+	return (GFARM_ERR_NO_ERROR);
+}
+
+static gfarm_error_t
+gfm_create_request(struct gfm_connection *gfm_server,
+	const char *path, int flags, gfarm_mode_t mode)
+{
+	gfarm_error_t e;
+	const char *base;
+
+	e = gfm_lookup_dir_request(gfm_server, path, &base);
+	if (e != GFARM_ERR_NO_ERROR)
+		return (e);
+	if (base[0] == '/' && base[1] == '\0') { /* "/" is special */
+		e = GFARM_ERR_IS_A_DIRECTORY;
+	} else {
+		e = gfm_client_create_request(gfm_server,
+		    base, flags, mode);
+	}
+	if (e != GFARM_ERR_NO_ERROR)
+		return (e);
+	return (gfm_client_get_fd_request(gfm_server));
+}
+
+static gfarm_error_t
+gfm_create_result(struct gfm_connection *gfm_server,
+	const char *path, gfarm_int32_t *fdp, int *typep)
+{
+	gfarm_error_t e;
+	const char *base;
+	int type;
+
+	e = gfm_lookup_dir_result(gfm_server, path, &base);
+	if (e != GFARM_ERR_NO_ERROR)
+		return (e);
+	if (base[0] == '/' && base[1] == '\0') { /* "/" is special */
+		/* shouldn't come here */
+		assert(0);
+		return (GFARM_ERR_IS_A_DIRECTORY);
+	} else {
+		gfarm_ino_t inum;
+		gfarm_uint64_t gen;
+		gfarm_mode_t mode;
+
+		e = gfm_client_create_result(gfm_server,
+		    &inum, &gen, &mode);
+		if (e != GFARM_ERR_NO_ERROR)
+			return (e);
+		type = gfs_mode_to_type(mode);
+	}
+	e = gfm_client_get_fd_result(gfm_server, fdp);
+	if (e != GFARM_ERR_NO_ERROR)
+		return (e);
+	if (typep != NULL)
+		*typep = type;
+	return (GFARM_ERR_NO_ERROR);
+}
+
+gfarm_error_t
+gfm_create_fd(const char *url, int flags, gfarm_mode_t mode,
+	int *fdp, int *typep)
+{
+	gfarm_error_t e;
+
+	if ((e = gfm_open_flag_check(flags)) != GFARM_ERR_NO_ERROR)
+		;
+	else if ((e = gfm_client_compound_begin_request(gfarm_metadb_server))
+	    != GFARM_ERR_NO_ERROR)
+		gflog_warning("compound_begin request: %s",
+		    gfarm_error_string(e));
+	else if ((e = gfm_create_request(gfarm_metadb_server,
+	    url, flags, mode)) != GFARM_ERR_NO_ERROR)
+		gflog_warning("create request: %s",
+		    gfarm_error_string(e));
+	else if ((e = gfm_client_compound_end_request(gfarm_metadb_server))
+	    != GFARM_ERR_NO_ERROR)
+		gflog_warning("compound_end request: %s",
+		    gfarm_error_string(e));
+
+	else if ((e = gfm_client_compound_begin_result(gfarm_metadb_server))
+	    != GFARM_ERR_NO_ERROR)
+		gflog_warning("compound_begin result: %s",
+		    gfarm_error_string(e));
+	else if ((e = gfm_create_result(gfarm_metadb_server, url,
+	    fdp, typep)) != GFARM_ERR_NO_ERROR)
+		gflog_warning("create_base request: %s",
+		    gfarm_error_string(e));
+	else if ((e = gfm_client_compound_end_result(gfarm_metadb_server))
+	    != GFARM_ERR_NO_ERROR)
+		gflog_warning("compound_end result: %s",
+		    gfarm_error_string(e));
+
+	return (e);
+}
+
+gfarm_error_t
+gfm_open_fd(const char *url, int flags,
+	int *fdp, int *typep)
+{
+	gfarm_error_t e;
+
+#if 0 /* not yet in gfarm v2 */
+	/* GFARM_FILE_EXCLUSIVE is a NOP with gfm_open_fd(). */
+	flags &= ~GFARM_FILE_EXCLUSIVE;
+#endif /* not yet in gfarm v2 */
+
+	if ((e = gfm_open_flag_check(flags)) != GFARM_ERR_NO_ERROR)
+		;
+	else if ((e = gfm_client_compound_begin_request(gfarm_metadb_server))
+	    != GFARM_ERR_NO_ERROR)
+		gflog_warning("compound_begin request: %s",
+		    gfarm_error_string(e));
+	else if ((e = gfm_open_request(gfarm_metadb_server, url, flags))
+	    != GFARM_ERR_NO_ERROR)
+		gflog_warning("open path request; %s",
+		    gfarm_error_string(e));
+	else if ((e = gfm_client_compound_end_request(gfarm_metadb_server))
+	    != GFARM_ERR_NO_ERROR)
+		gflog_warning("compound_end request: %s",
+		    gfarm_error_string(e));
+
+	else if ((e = gfm_client_compound_begin_result(gfarm_metadb_server))
+	    != GFARM_ERR_NO_ERROR)
+		gflog_warning("compound_begin result: %s",
+		    gfarm_error_string(e));
+	else if ((e = gfm_open_result(gfarm_metadb_server, url,
+	    fdp, typep)) != GFARM_ERR_NO_ERROR) {
+#if 0
+		gflog_warning("open path result: %s",
+		    gfarm_error_string(e));
+#endif
+	} else if ((e = gfm_client_compound_end_result(gfarm_metadb_server))
+	    != GFARM_ERR_NO_ERROR)
+		gflog_warning("compound_end result: %s",
+		    gfarm_error_string(e));
+
+	return (e);
+}
+
+gfarm_error_t
+gfm_close_fd(int fd)
+{
+	gfarm_error_t e;
+
+	if ((e = gfm_client_compound_begin_request(gfarm_metadb_server))
+	    != GFARM_ERR_NO_ERROR)
+		gflog_warning("compound_begin request: %s",
+		    gfarm_error_string(e));
+	else if ((e = gfm_client_put_fd_request(gfarm_metadb_server, fd))
+	    != GFARM_ERR_NO_ERROR)
+		gflog_warning("put_fd request: %s",
+		    gfarm_error_string(e));
+	else if ((e = gfm_client_close_request(gfarm_metadb_server)
+	    ) != GFARM_ERR_NO_ERROR)
+		gflog_warning("close request: %s",
+		    gfarm_error_string(e));
+	else if ((e = gfm_client_compound_end_request(gfarm_metadb_server))
+	    != GFARM_ERR_NO_ERROR)
+		gflog_warning("compound_end request: %s",
+		    gfarm_error_string(e));
+
+	else if ((e = gfm_client_compound_begin_result(gfarm_metadb_server))
+	    != GFARM_ERR_NO_ERROR)
+		gflog_warning("compound_begin result: %s",
+		    gfarm_error_string(e));
+	else if ((e = gfm_client_put_fd_result(gfarm_metadb_server))
+	    != GFARM_ERR_NO_ERROR)
+		gflog_warning("put_fd result: %s",
+		    gfarm_error_string(e));
+	else if ((e = gfm_client_close_result(gfarm_metadb_server))
+	    != GFARM_ERR_NO_ERROR)
+		gflog_warning("close result: %s",
+		    gfarm_error_string(e));
+	else if ((e = gfm_client_compound_end_result(gfarm_metadb_server))
+	    != GFARM_ERR_NO_ERROR)
+		gflog_warning("compound_end result: %s",
+		    gfarm_error_string(e));
+
+	return (e);
+}
---
+++ gfarm_v2/branches/2.1.2/server/gfmd/subr.c
@@ -0,0 +1,168 @@
+#include <pthread.h>
+
+#include <assert.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+
+#define GFARM_INTERNAL_USE
+#include <gfarm/error.h>
+#include <gfarm/gfarm_misc.h>
+#include <gfarm/gfs.h>
+
+#include "gfutil.h"
+#include "gfp_xdr.h"
+#include "auth.h"
+
+#include "thrsubr.h"
+#include "subr.h"
+#include "peer.h"
+
+int debug_mode = 0;
+
+static pthread_mutex_t giant_mutex;
+
+void
+giant_init(void)
+{
+	mutex_init(&giant_mutex, "giant_init", "giant");
+}
+
+void
+giant_lock(void)
+{
+	mutex_lock(&giant_mutex, "giant_lock", "giant");
+}
+
+void
+giant_unlock(void)
+{
+	mutex_unlock(&giant_mutex, "giant_unlock", "giant");
+}
+
+pthread_attr_t gfarm_pthread_attr;
+
+void
+gfarm_pthread_attr_init(void)
+{
+	int err;
+
+	err = pthread_attr_init(&gfarm_pthread_attr);
+	if (err != 0)
+		gflog_fatal("pthread_attr_init(): %s", strerror(err));
+	err = pthread_attr_setdetachstate(&gfarm_pthread_attr,
+	    PTHREAD_CREATE_DETACHED);
+	if (err != 0)
+		gflog_fatal("PTHREAD_CREATE_DETACHED: %s", strerror(err));
+	gfarm_pthread_attr_setstacksize(&gfarm_pthread_attr);
+}
+
+
+pthread_attr_t *
+gfarm_pthread_attr_get(void)
+{
+	static pthread_once_t gfarm_pthread_attr_initialized =
+	    PTHREAD_ONCE_INIT;
+
+	pthread_once(&gfarm_pthread_attr_initialized,
+	    gfarm_pthread_attr_init);
+
+	return (&gfarm_pthread_attr);
+}
+
+gfarm_error_t
+create_detached_thread(void *(*thread_main)(void *), void *arg)
+{
+	int err;
+	pthread_t thread_id;
+
+	err = pthread_create(&thread_id, gfarm_pthread_attr_get(),
+	    thread_main, arg);
+	return (err == 0 ? GFARM_ERR_NO_ERROR : gfarm_errno_to_error(err));
+}
+
+int
+accmode_to_op(gfarm_uint32_t flag)
+{
+	int op;
+
+	switch (flag & GFARM_FILE_ACCMODE) {
+	case GFARM_FILE_RDONLY:	op = GFS_R_OK; break;
+	case GFARM_FILE_WRONLY:	op = GFS_W_OK; break;
+	case GFARM_FILE_RDWR:	op = GFS_R_OK|GFS_W_OK; break;
+	case GFARM_FILE_LOOKUP:	op = 0; break;
+	default:
+		assert(0);
+		op = 0;
+	}
+	return op;
+}
+
+gfarm_error_t
+gfm_server_get_request(struct peer *peer, const char *diag,
+	const char *format, ...)
+{
+	va_list ap;
+	gfarm_error_t e;
+	int eof;
+	struct gfp_xdr *client = peer_get_conn(peer);
+
+	if (debug_mode)
+		gflog_info("<%s> start receiving", diag);
+
+	va_start(ap, format);
+	e = gfp_xdr_vrecv(client, 0, &eof, &format, &ap);
+	va_end(ap);
+
+	if (e != GFARM_ERR_NO_ERROR) {
+		gflog_warning("%s: %s", diag, gfarm_error_string(e));
+		peer_record_protocol_error(peer);
+		return (e);
+	}
+	if (eof) {
+		gflog_warning("%s: missing RPC argument", diag);
+		peer_record_protocol_error(peer);
+		return (GFARM_ERR_PROTOCOL);
+	}
+	if (*format != '\0')
+		gflog_fatal("%s: invalid format character to get request",
+		    diag);
+	return (GFARM_ERR_NO_ERROR);
+}
+
+gfarm_error_t
+gfm_server_put_reply(struct peer *peer, const char *diag,
+	gfarm_error_t ecode, const char *format, ...)
+{
+	va_list ap;
+	gfarm_error_t e;
+	struct gfp_xdr *client = peer_get_conn(peer);
+
+	if (debug_mode)
+		gflog_info("<%s> sending reply: %d", diag, (int)ecode);
+
+	va_start(ap, format);
+	e = gfp_xdr_send(client, "i", (gfarm_int32_t)ecode);
+	if (e != GFARM_ERR_NO_ERROR) {
+		va_end(ap);
+		gflog_warning("%s: %s", diag, gfarm_error_string(e));
+		peer_record_protocol_error(peer);
+		return (e);
+	}
+	if (ecode == GFARM_ERR_NO_ERROR) {
+		e = gfp_xdr_vsend(client, &format, &ap);
+		if (e != GFARM_ERR_NO_ERROR) {
+			va_end(ap);
+			gflog_warning("%s: %s", diag, gfarm_error_string(e));
+			peer_record_protocol_error(peer);
+			return (e);
+		}
+		if (*format != '\0')
+			gflog_fatal("%s: %s", diag,
+			    "invalid format character to put reply");
+	}
+	va_end(ap);
+	/* do not call gfp_xdr_flush() here for a compound protocol */
+
+	return (ecode);
+}
