35fa1fc6c3d8340f548cda4a9362fa762437b08b
tatebe <tatebe@4a5e1ece-6a47-475c-b646-731d720b41d0>
2010-05-10 08:49:26 +0000

    Gfarm version 2.4.0 release candidate 5

---
+++ gfarm_v2/tags/gfarm_2_4_0_rc5/server/gfmd/subr.h
@@ -0,0 +1,19 @@
+extern int debug_mode;
+
+void giant_init(void);
+void giant_lock(void);
+void giant_unlock(void);
+
+void gfarm_pthread_attr_setstacksize(pthread_attr_t *attr);
+gfarm_error_t create_detached_thread(void *(*)(void *), void *);
+
+char *strdup_ck(const char *, const char *);
+char *strdup_log(const char *, const char *);
+
+int accmode_to_op(gfarm_uint32_t);
+
+struct peer;
+gfarm_error_t gfm_server_get_request(struct peer *, const char *,
+	const char *, ...);
+gfarm_error_t gfm_server_put_reply(struct peer *, const char *,
+	gfarm_error_t, const char *, ...);
