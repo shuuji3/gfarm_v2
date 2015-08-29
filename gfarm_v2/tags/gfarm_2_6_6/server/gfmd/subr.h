979ebc074fed391e5717d54c2d9396b343a08de3
tatebe <tatebe@4a5e1ece-6a47-475c-b646-731d720b41d0>
2015-08-29 15:06:33 +0000

    Gfarm version 2.6.6 release

---
+++ gfarm_v2/tags/gfarm_2_6_6/server/gfmd/subr.h
@@ -0,0 +1,16 @@
+extern int debug_mode;
+
+void giant_init(void);
+void giant_lock(void);
+int giant_trylock(void);
+void giant_unlock(void);
+
+gfarm_error_t create_detached_thread(void *(*)(void *), void *);
+gfarm_error_t gfarm_pthread_set_priority_minimum(const char *);
+
+char *strdup_ck(const char *, const char *);
+char *strdup_log(const char *, const char *);
+
+int accmode_to_op(gfarm_uint32_t);
+
+gfarm_uint64_t trace_log_get_sequence_number(void);
