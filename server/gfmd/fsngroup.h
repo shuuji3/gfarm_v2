27ba1f08eb6519e96b8067d0ee835f27fd1c5c62
n-soda <n-soda@4a5e1ece-6a47-475c-b646-731d720b41d0>
2018-11-26 17:57:18 +0000

    branch for #1044 - make replica_check faster

--- server/gfmd/fsngroup.h
+++ server/gfmd/fsngroup.h
@@ -9,10 +9,13 @@ gfarm_error_t fsngroup_get_hosts(const char *, int *, struct host ***);
  * Replication scheduler:
  */
 struct inode;
+struct dirset;
 struct file_copy;
+struct hostset;
 gfarm_error_t fsngroup_schedule_replication(
-	struct inode *, const char *, int, struct host **,
-	int *, struct host **, gfarm_time_t, int *, struct host **,
+	struct inode *, struct dirset *tdirset,
+	const char *, int, struct host **,
+	int *, struct hostset *, gfarm_time_t, int *, struct hostset *,
 	const char *, int *, int *);
 
 /*
@@ -20,8 +23,8 @@ gfarm_error_t fsngroup_schedule_replication(
  */
 struct peer;
 gfarm_error_t gfm_server_fsngroup_get_all(
-	struct peer *, gfp_xdr_xid_t, size_t *, int, int);
+	struct peer *, int, int);
 gfarm_error_t gfm_server_fsngroup_get_by_hostname(
-	struct peer *, gfp_xdr_xid_t, size_t *, int, int);
+	struct peer *, int, int);
 gfarm_error_t gfm_server_fsngroup_modify(
-	struct peer *, gfp_xdr_xid_t, size_t *, int, int);
+	struct peer *, int, int);
